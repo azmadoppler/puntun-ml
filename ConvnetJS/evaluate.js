f0e5810cba01b72e1656537eb5d9f5d5dd341ad3
Thanawit Gerdprasert <thanawit.g@ku.th>
2018-04-26 02:53:33 +0700

    added gitignore

--- ConvnetJS/evaluate.js
+++ ConvnetJS/evaluate.js
@@ -1,20 +1,59 @@
 var convnetjs = require('convnetjs')
 var fs = require('fs');
+var data = require('../training_song.json')
+var old_data = require('./autoencoder_model.json')
+
+var fs = require('fs');
+
+let train_set = [];
+
+//extract 
+data.forEach(function(el){
+    let waveMax = JSON.parse(el.waveMax)
+    let waveMin = JSON.parse(el.waveMin)
+    if( waveMax.length != 1024){
+      waveMax.length = 1024
+      waveMin.length = 1024
+    }
+    let waveMean = []
+    
+    for (let index = 0; index < waveMax.length; index++) {
+      let curentMean = (waveMax[index] + waveMin[index])/2.0
+      waveMean.push(curentMean)
+      
+    }
+    
+    el.waveMean = waveMean
+  })
+
+  data.forEach(element => {
+    let temp_vol =  new convnetjs.Vol(1024, 1, 1);
+    for(let i = 0 ; i < element.waveMean.length ; i++){
+        temp_vol.w[i] = element.waveMean[i]
+    }
+    train_set.push(temp_vol)
+});
+
 
 var json;
 
-fs.readFile('data.txt', 'utf8', function (err,data) {
+fs.readFile('autoencoder_model.json', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
-  json = JSON.parse(data)
+  // console.log(data);
+  json = JSON.parse(old_data)
   var net = new convnetjs.Net(); 
   net.fromJSON(json)
-  var x = new convnetjs.Vol([0.5, -1.3]);
-  var probability_volume = net.forward(x);
-  console.log('probability that x is class 0: ' + probability_volume.w[1]);
   
   
+  var trainer = new convnetjs.SGDTrainer(net, {learning_rate:1, method:'adadelta', batch_size:10, l2_decay:0.001});
+  
+  
+  // let prob = net.forward(train_set[0])
+
+  // console.log(prob)
+
+
 });
 // console.log(json)
--- ConvnetJS/test_input.js
+++ ConvnetJS/test_input.js
@@ -27,7 +27,7 @@ data.forEach(function(el){
 
 
 data.forEach(element => {
-    let temp_vol =  new convnetjs.Vol(1500, 1, 1);
+    let temp_vol =  new convnetjs.Vol(1024, 1, 1);
     for(let i = 0 ; i < element.waveMean.length ; i++){
         temp_vol.w[i] = element.waveMean[i]
     }
@@ -36,20 +36,22 @@ data.forEach(element => {
 
 layer_defs = [];
 layer_defs.push({type:'input', out_sx:1024, out_sy:1, out_depth:1});
-layer_defs.push({type:'conv', sx:3, filters:8, stride:1, pad:1, activation:'relu'});
-layer_defs.push({type:'pool', sx:2 ,sy:1, stride:2});
-layer_defs.push({type:'conv', sx:3, filters:16, stride:1, pad:1, activation:'relu'});
-layer_defs.push({type:'softmax', num_classes:64});
+// layer_defs.push({type:'conv', sx:3, filters:8, stride:1, pad:1, activation:'relu'});
+// layer_defs.push({type:'pool', sx:2 ,sy:1, stride:2});
+// layer_defs.push({type:'conv', sx:3, filters:16, stride:1, pad:1, activation:'relu'});
+// layer_defs.push({type:'pool', sx:2 ,sy:1, stride:2});
+layer_defs.push({type:'fc', num_neurons:3, activation:'relu'}); 
+// layer_defs.push({type:'softmax', num_classes:3});
 
 net = new convnetjs.Net();
 net.makeLayers(layer_defs);
 
-
+console.log(train_set[0])
 
 trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:10, l2_decay:0.001});
 
 for(let i = 0 ; i < train_set.length ; i++){
-    let stat = trainer.train(train_set[i])
+    let stat = trainer.train(train_set[i],0)
     console.log(stat)
 }
 
